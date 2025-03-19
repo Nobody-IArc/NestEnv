f1fb265e2ce14fb157b499e856e1e28c845d6b2e
Nobody-IArc <utk3970@naver.com>
2025-03-19 21:30:21 +0900

    .env와 환경 변수

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // 환경 변수 설정 관련 모듈
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({})],
   controllers: [AppController],
   providers: [AppService],
 })
